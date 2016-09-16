b5b9b4e8fdc950493c1219478295e72185916583
Fang Dehui <javaeecoding@gmail.com>
2016-09-16 16:59:53 +0900

    add workshop

--- src/main/resources/public/admin/datatables/my.datatables.js
+++ src/main/resources/public/admin/datatables/my.datatables.js
@@ -91,3 +91,17 @@ $.myCommonUIEvent = function() {
         $('.uiDetailPage').hide();
     }
 };
+
+
+$.readImage = function (input, $targetImg) {
+
+    if (input.files && input.files[0]) {
+        var reader = new FileReader();
+
+        reader.onload = function (e) {
+            $targetImg.attr('src', e.target.result);
+        }
+
+        reader.readAsDataURL(input.files[0]);
+    }
+}
